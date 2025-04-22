f755dd97e5e3ccd73a034f7b8e80e6cb7a56e885
faizyi <faizi13062000@gmail.com>
2025-04-22 16:05:58 +0300

    server url

--- src/axios/axios.jsx
+++ src/axios/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8001',
+    baseURL: 'https://polling-app-server-production.up.railway.app',
     headers: {
         'Content-Type': 'application/json',
     },
